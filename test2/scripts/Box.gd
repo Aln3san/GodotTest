e399b5ecd88bdee8173353bcab3bc4e98fc663e7
Aln3san <anas.aln3san@gmail.com>
2025-03-21 17:35:55 +0200

    Add files via upload

--- test2/scripts/Box.gd
+++ test2/scripts/Box.gd
@@ -9,24 +9,12 @@ var inputs = {
 	'ui_right': Vector2.RIGHT,
 }
 
-func _unhandled_input(event):
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(dir)
-	pass
 
 func move(dir):
 	var vector_pos = inputs[dir] * 16
 	ray.cast_to = vector_pos
 	ray.force_raycast_update()
-
 	if !ray.is_colliding():
 		position += vector_pos
 		return true
-	else:
-		var collider = ray.get_collider()
-		if collider.is_in_group("Box"):
-			if collider.move(dir):
-				position += vector_pos
-				return true
 	return false
